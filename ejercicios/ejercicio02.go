c51dbe9ea774f9c926455e51b660ebfede51ad12
Ernesto Gamez <ernesto.gamez@tangelogames.com>
2023-06-13 17:40:54 +0200

    refactor exercise 02

--- ejercicios/ejercicio02.go
+++ ejercicios/ejercicio02.go
@@ -8,29 +8,26 @@ import (
 )
 
 var max int = 10
+var number int
+var err error
 
 func TablaMultiplicar() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Println("Introduce un numbero del 1 al 10: ")
 		if scanner.Scan() {
-			number, err := strconv.Atoi(scanner.Text())
+			number, err = strconv.Atoi(scanner.Text())
 			if err != nil {
 				fmt.Println("Error, no es un número.")
 				continue
-			}
-			if number > 1 && number <= 10 {
-				for i := 0; i <= max; i++ {
-					result := number * i
-					fmt.Printf("%d x %d = %d \n", number, i, result)
-				}
-
 			} else {
-				fmt.Println("Solo imprimo las tablas del 1 al 10")
-				continue
+				break
 			}
-			break
 		}
-
 	}
+	for i := 0; i <= max; i++ {
+		result := number * i
+		fmt.Printf("%d x %d = %d \n", number, i, result)
+	}
+
 }
